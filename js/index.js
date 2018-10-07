e07705e86a7b3867e3f8080dc05d89c756d6418a
rishav <96rishu.nidhi@gmail.com>
2018-10-07 10:10:35 +0530

    added js folder

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+/* practicing variable-rishav 7th sept */
+var a;
+console.log(a);
+var a = "rishav";
+console.log(a);
+document.write("how r u doing today?");
+document.write("I m good");
+document.write("how r u doing today?");
+/* document.write("I m good");
+document.write("how r u doing today?");
+document.write("I m good");
+document.write("how r u doing today?");
+document.write("I m good");
+*/
+document.write("how r u doing today?");
+document.write("I m good");
